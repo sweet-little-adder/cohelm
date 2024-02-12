b1dc03430036453e445d8c9898796fb67ad613a9
sweet-little-adder <orioncheetah@gmail.com>
2024-02-12 14:34:03 -0500

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#202124] space-y-9">
+		<main className="flex min-h-screen flex-col items-center justify-center p-24 bg-[#202124] space-y-9">
 			<div className="w-full bg-white rounded-md p-6 shadow-md">
 				<UploadForm />
 			</div>
-
 			<div className="text-black w-full bg-white rounded-md p-6 shadow-md">
 				Decision
 			</div>
