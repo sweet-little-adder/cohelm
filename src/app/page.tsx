fa6cb1bb15a354fdf422db0a7d98f93913925649
sweet-little-adder <orioncheetah@gmail.com>
2024-02-12 18:35:22 -0500

    mock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,70 @@ import UploadForm from "@/components/uploadform";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { MdOutlineDone } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
 
-export default function Home() {
+interface Props {
+	approve: React.Dispatch<React.SetStateAction<boolean | null>>;
+}
+
+const Home: React.FC<Props> = () => {
 	const [loadLLM, setLoadLLM] = useState<boolean>(false);
-	const [approve, setApprove] = useState<boolean>(false);
+	const [approve, setApprove] = useState<boolean | null>(null);
+	function scrollPageDown(pixels: number) {
+		window.scrollTo({
+			top: window.pageYOffset + pixels,
+			behavior: "smooth", // Optional: Smooth scrolling behavior
+		});
+	}
 	useEffect(() => {
 		if (loadLLM) {
 			console.log("fetch");
+			scrollPageDown(1000);
 		}
 	}, [loadLLM]);
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-24 bg-[#202124] space-y-9 text-[#343541] ">
-			<div className="w-full bg-white rounded-md p-6 shadow-md">
+		<main className="flex min-h-screen flex-col items-start justify-start p-24 bg-[#202124] space-y-9 text-[#343541] transition-all">
+			<div className="transition-all w-full bg-white rounded-md p-6 shadow-md">
 				<UploadForm setLoadLLM={setLoadLLM} />
 			</div>
-			<div className=" w-full bg-white rounded-md p-6 shadow-md">
-				<p>Decision Process</p>
-				<div>{loadLLM && <ProcessDocument setApprove={setApprove} />}</div>
-			</div>
-			<div className="flex-col text-center space-y-5 w-full bg-white rounded-md p-6 shadow-md">
-				<p>Approve</p>
-				{approve && (
-					<MdOutlineDone
-						color={"#fff"}
-						size={22}
-						className="p-1 mx-auto bg-[#049E67] rounded-full"
-					/>
-				)}
-			</div>
+			{loadLLM && (
+				<div
+					className={`transition-all opacity-0 transition-opacity delay-500 ease-in-out  ${
+						loadLLM && `opacity-100`
+					} w-full bg-white rounded-md p-6 shadow-md`}
+				>
+					<ProcessDocument setApprove={setApprove} />
+				</div>
+			)}
+
+			{approve !== null && (
+				<div
+					className={`flex-col text-center space-y-5 w-full bg-white rounded-md p-6 shadow-md`}
+				>
+					<p>{approve ? "Approved" : "Not Approved"}</p>
+
+					{approve ? (
+						<MdOutlineDone
+							color={"#fff"}
+							size={32}
+							className={`transform transition scale-0 ${
+								approve && `scale-100`
+							} duration-500  ease-in-out p-1 mx-auto bg-[#049E67] rounded-full`}
+						/>
+					) : (
+						<RxCross2
+							color={"#fff"}
+							size={32}
+							className={`transform transition scale-0 ${
+								!approve && `scale-100`
+							} duration-500  ease-in-out p-1 mx-auto bg-[#E91E63] rounded-full`}
+						/>
+					)}
+				</div>
+			)}
 		</main>
 	);
-}
+};
+
+export default Home;
--- src/components/processDocument.tsx
+++ src/components/processDocument.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import axios from "axios";
 
 interface Props {
-	setApprove: React.Dispatch<React.SetStateAction<boolean>>;
+	setApprove: React.Dispatch<React.SetStateAction<boolean | null>>;
 }
 
 const ProcessDocument: React.FC<Props> = ({ setApprove }) => {
 	const [data, setData] = useState<any>(null);
 	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<Error | null>(null);
+	const [expanded, setExpanded] = useState<boolean>(false);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -21,19 +22,25 @@ const ProcessDocument: React.FC<Props> = ({ setApprove }) => {
 				setError(error as Error);
 			} finally {
 				setTimeout(() => setLoading(false), 3000);
-				console.log("Data: ", data);
-				setTimeout(() => setApprove(true), 9000);
 			}
 		};
 
 		fetchData();
+		console.log("Data: ", data);
 	}, []);
+
+	useEffect(() => {
+		if (data !== null) {
+			console.log("Data: ", data);
+			setTimeout(() => setApprove(data.is_met), 3000);
+		}
+	}, [data]);
 
 	return (
 		<div>
+			{console.log("Data: ", data)}
 			{loading && (
 				<div className="flex items-center">
-					{/* <p>Loading</p> */}
 					<div className="lds-ellipsis translate-x-0 my-9 mx-auto">
 						<div></div>
 						<div></div>
@@ -44,29 +51,27 @@ const ProcessDocument: React.FC<Props> = ({ setApprove }) => {
 			)}
 			{error && <p>Error: {error.message}</p>}
 			{data && !loading && (
-				<div>
-					<p>Data:</p>
-					procedure_name: {data.procedure_name}
-					<br />
-					summary: {data.summary}
-					<br />
-					is_met: {data.is_met}
-					<br />
-					question: {data.question}
-					<br />
-					options: {data.options}
-					<br />
-					reasoning: {data.reasoning}
-					<br />
-					decision: {data.decision}
-					<br />
-					is_final: {data.is_final}
-					<br />
-					evidence: {data.evidence}
-					<br />
-					question: {data.question}
-					<br />
-					{/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+				<div className="space-y-5">
+					<div className="border-b-[1px] pb-5">
+						<p>Procedure</p> <b>{data.procedure_name}</b>
+					</div>
+					<div className="text-sm text-[#4a4a4a] leading-loose tracking-wide">
+						{expanded ? data.summary : `${data.summary.slice(0, 100)}`}
+
+						<span
+							className="text-[#9e9e9e] hover:text-[#c9c9c9]  ml-1 cursor-pointer"
+							onClick={() => setExpanded(!expanded)}
+						>
+							{expanded ? `[collapse]` : `[...]`}
+						</span>
+					</div>
+					<div className="border-b-[1px] pb-5">
+						Does the patient meet requirement: {data.is_met ? "Yes" : "No"}
+					</div>
+					<div className="border-b-[1px] pb-5">question: {data.question}</div>
+					<div className="border-b-[1px] pb-5">options: {data.options}</div>
+					<div className="">reasoning: {data.reasoning}</div>
+					{/* <div className="">decision: {data.decision}</div> */}
 				</div>
 			)}
 		</div>
